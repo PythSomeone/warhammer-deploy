dbbd4fd1d0aa874ec72f03b35dc4aea1745746ab
PythSomeone <40042814+PythSomeone@users.noreply.github.com>
2021-05-10 18:52:08 +0200

    fix

--- warhammer/settings.py
+++ warhammer/settings.py
@@ -129,12 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
